b56c5b66a815c92234ac23738161c346b43f0c75
VadimTabelskij <bagum94@gmail.com>
2023-03-09 19:05:53 +0200

    feat: create cities map script

--- sql/scripts/create-cities.js
+++ sql/scripts/create-cities.js
@@ -4,12 +4,34 @@ const { countryMap } = require('./create-countries');
 const { cars } = dbData;
 
 const insertionRows = cars
-  .map(x => [countryMap[x.location.country], x.location.city])
-  .map(([id, title]) => `(${id}, '${title}')`)
-  .join(',\n');
+.map(x => ({
+  countryId: countryMap[x.location.country],
+  title: x.location.city
+}))
+.map(({ countryId, title }) => `(${countryId}, '${title}')`)
 
 const insertionSQL = `
 insert into city(countryId, title) values
-${insertionRows};
-`
+${insertionRows};`
+
+const countryCityMap = cars
+  .map(x => x.location)
+  .reduce((prevMap, { country, city }, i) => {
+    const nextMap = {
+      ...prevMap,
+    }
+
+    if (nextMap[country] === undefined) {
+      nextMap[country] = {};
+    }
+
+    nextMap[country][city] = i + 1
+    return nextMap
+  }, {})
+
 console.log(insertionSQL)
+console.log(countryCityMap)
+
+module.exports = {
+  countryCityMap,
+}

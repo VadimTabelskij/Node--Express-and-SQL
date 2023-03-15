6dee8448d49eb04b26d6f4ce7049cca3396a70fd
VadimTabelskij <bagum94@gmail.com>
2023-03-15 22:33:51 +0200

    feat: created router for auth

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import config from 'config';
 import cors from 'cors';
 import carsController from 'controllers/cars';
 import DatabaseService from 'services/database-service';
+import authController from 'controllers/auth';
 
 const server = express();
 server.use(cors());
@@ -11,6 +12,7 @@ server.use(express.json());
 server.use(morgan('tiny'));
 server.use(express.static('public'));
 server.use('/api/cars', carsController);
+server.use('/api/auth', authController);
 
 DatabaseService.connect(() => {
   server.listen(config.server.port, () => {

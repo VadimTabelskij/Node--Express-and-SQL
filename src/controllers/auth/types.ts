c6ca0dff3272ffa7af4225f660fb1da704c80f3b
VadimTabelskij <bagum94@gmail.com>
2023-03-16 21:03:20 +0200

    feat: login method  and validation schema

--- src/controllers/auth/types.ts
+++ src/controllers/auth/types.ts
@@ -1,20 +1,27 @@
-export type RegistrationBody = {
-    email: string,
-    password: string,
-    passwordConfirmation: string,
-    name: string,
-    surname: string,
-    phone: string,
-    image: string,
-  };
+export type UserEntity = {
+  id: number,
+  email: string,
+  password: string,
+  name: string,
+  surname: string,
+  phone: string,
+  image: string,
+};
 
-  export type UserData = Omit<RegistrationBody, 'passwordConfirmation'>;
+export type RegistrationBody = Omit<UserEntity, 'id'> & {
+  passwordConfirmation: string,
+};
 
-  export type UserViewModel = Omit<RegistrationBody, 'password' | 'passwordConfirmation'> & {
-    id: number,
-  };
+export type Credentials = {
+  email: string,
+  password: string,
+};
 
-  export type AuthResponse = {
-    user: UserViewModel,
-    token: string
-  };
+export type UserData = Omit<RegistrationBody, 'passwordConfirmation'>;
+
+export type UserViewModel = Omit<UserEntity, 'password'>;
+
+export type AuthResponse = {
+  user: UserViewModel,
+  token: string
+};

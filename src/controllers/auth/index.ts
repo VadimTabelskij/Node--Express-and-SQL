bd03e39b297d112b05e8f4d7a9618e1b607b4823
VadimTabelskij <bagum94@gmail.com>
2023-03-17 19:45:02 +0200

    feat: implemed auth by token method

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
+import jwtTokenMiddleware from 'middlewares/jwt-token-middleware';
 import { auth } from './methods/auth';
 import { login } from './methods/login';
 import { register } from './methods/register';
 
 const authController = Router();
 
-authController.post('/auth', auth);
-authController.post('/login', login);
 authController.post('/register', register);
+authController.post('/login', login);
+authController.post('/auth', jwtTokenMiddleware, auth);
 
 export default authController;

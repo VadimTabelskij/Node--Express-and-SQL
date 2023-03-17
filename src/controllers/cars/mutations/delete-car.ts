714199652d19cd9cc83272618846e69119838aa0
VadimTabelskij <bagum94@gmail.com>
2023-03-17 11:04:47 +0200

    feat: implemented protection  deleting and updating mutations

--- src/controllers/cars/mutations/delete-car.ts
+++ src/controllers/cars/mutations/delete-car.ts
@@ -3,6 +3,7 @@ import ServerSetupError from 'errors/server-setup-error';
 import handleRequestError from 'helpers/handle-request-error';
 import { CarViewModel } from 'controllers/cars/types';
 import CarModel from 'controllers/cars/cars-model';
+import ForbiddenError from 'errors/forbidden-error';
 
 const deleteCar: RequestHandler<
   { id?: string },
@@ -13,8 +14,12 @@ const deleteCar: RequestHandler<
   const { id } = req.params;
 
   try {
-    if (id === undefined) throw new ServerSetupError();
+    if (id === undefined || req.authUser === undefined) throw new ServerSetupError();
     const carViewModel = await CarModel.getCar(id);
+
+    if (req.authUser.role !== 'ADMIN' && req.authUser.id !== carViewModel.person.id) {
+      throw new ForbiddenError();
+    }
     await CarModel.deleteCar(id);
 
     res.status(200).json(carViewModel);
--- src/controllers/cars/mutations/put-car.ts
+++ src/controllers/cars/mutations/put-car.ts
@@ -3,6 +3,7 @@ import ServerSetupError from 'errors/server-setup-error';
 import handleRequestError from 'helpers/handle-request-error';
 import { CarViewModel, CarDataBody } from 'controllers/cars/types';
 import carDataValidationSchema from 'controllers/cars/validation-schemas/car-data-validation-schema';
+import ForbiddenError from 'errors/forbidden-error';
 import CarModel from '../cars-model/index';
 
 const putCar: RequestHandler<
@@ -13,9 +14,14 @@ const putCar: RequestHandler<
 > = async (req, res) => {
   const { id } = req.params;
 
-  if (id === undefined) throw new ServerSetupError();
-
   try {
+    if (id === undefined || req.authUser === undefined) throw new ServerSetupError();
+    const carToUpdate = await CarModel.getCar(id);
+
+    if (req.authUser.role !== 'ADMIN' && req.authUser.id !== carToUpdate.person.id) {
+      throw new ForbiddenError();
+    }
+
     const carData = carDataValidationSchema.validateSync(req.body);
     const carViewModel = await CarModel.replaceCar(id, carData);
 
